87f65da6be14eb929427e5c51c0809a5be304a2a
CALVINCE <calvince.ogwel@gmail.com>
2023-02-10 10:48:40 +0300

    completed compulsory lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,72 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness=(value)
+        @happiness = [[value, 0].max, 10].min
+      end
+
+    def hygiene=(value)
+        @hygiene = [[value, 0].max, 10].min
+      end
+
+    def happy?
+        if happiness > 7
+            return true
+        end
+            return false
+    end
+
+    def clean?
+        if hygiene > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_paid(amount)
+        @bank_account += amount
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene +=4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -=3
+        self.happiness +=2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness +=3
+        friend.happiness +=3
+          "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend,topic)
+        if topic == "politics"
+            self.happiness -=2
+            friend.happiness -=2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness +=1
+            friend.happiness +=1
+            return "blah blah sun blah rain"
+
+        else
+            return "blah blah blah blah blah"
+        end
+    end
+end
+
+
